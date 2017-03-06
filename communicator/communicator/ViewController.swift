943d509886f6b134fbe11f95b9a75044ca98aa03
Morgan Morley Mills <morganmorleymills@Morgans-MacBook-Pro.local>
2017-03-06 10:23:34 -0500

    auth simplification and code commented

--- communicator/communicator/ViewController.swift
+++ communicator/communicator/ViewController.swift
@@ -5,37 +5,41 @@
 //  Created by Morgan Morley Mills on 3/6/17.
 //  Copyright © 2017 Morgan Morley Mills. All rights reserved.
 //
+//  sources:
+//      Firebase Tutorials for iOS Apps Playlist by CodeWithChris (Youtube.com)
 
 import UIKit
 import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    
+    // button to toggle between login and create account views
     @IBOutlet weak var loginSelector: UISegmentedControl!
 
+    // email address
     @IBOutlet weak var emailTextField: UITextField!
     
-    @IBOutlet weak var usernameLabel: UILabel!
-    
-    @IBOutlet weak var usernameTextField: UITextField!
-    
+    // password confirmation label for hiding on login view
     @IBOutlet weak var confirmLabel: UILabel!
     
-    @IBOutlet weak var confirmTextField: UITextField!
-    
+    // password
     @IBOutlet weak var passwordTextField: UITextField!
     
+    // password confirmation
+    @IBOutlet weak var confirmTextField: UITextField!
+    
+    // button to login or sign up
     @IBOutlet weak var loginButton: UIButton!
     
+    // boolean storing whether the login view is currently active
     var isLogin: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        usernameLabel.isHidden = true
-        usernameTextField.isHidden = true
         confirmLabel.isHidden = true
         confirmTextField.isHidden = true
+        print("welcome")
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,73 +47,62 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // switches between login and create account views and buttons
     @IBAction func loginSelectorChanged(_ sender: Any) {
-        // Change the boolean
+        // change the boolean
         isLogin = !isLogin
         
-        // Check isLogin and set the button and labels
+        // check isLogin and set the button and password confirmation prompts
         if isLogin {
             loginButton.setTitle("Login", for: .normal)
-            usernameLabel.isHidden = true
-            usernameTextField.isHidden = true
             confirmLabel.isHidden = true
             confirmTextField.isHidden = true
         } else {
             loginButton.setTitle("Sign Up", for: .normal)
-            usernameLabel.isHidden = false
-            usernameTextField.isHidden = false
             confirmLabel.isHidden = false
             confirmTextField.isHidden = false
         }
     }
 
+    // logs into or registers user with Firebase
     @IBAction func loginButtonTapped(_ sender: UIButton) {
-        // TODO - Form validation on email and password
+        // TODO - Form validation on email and password - separate function
         if let email = emailTextField.text, let pass = passwordTextField.text {
-            // Check isLogin to sign in or register the user with Firebase
+            // check if login or create account view is active
             if isLogin {
-                confirmedLogin(email: email, pass: pass)
+                // sign in with Firebase
+                FIRAuth.auth()?.signIn(withEmail: email, password: pass, completion: { (user, error) in
+                    //Check that user isn't nil
+                    if let u = user {
+                        // go to roster
+                        self.performSegue(withIdentifier: "goToRoster", sender: self)
+                    } else {
+                        print("an error occured \(error)")
+                    }
+                })
             } else {
-                if let confirmation = confirmTextField.text, let username = usernameTextField.text {
+                if let confirmation = confirmTextField.text {
                     if pass == confirmation {
-                        confirmedRegister(email: email, pass: pass, confirmation: confirmation)
-                        // add username to user account
+                        //sign up with Firebase
+                        FIRAuth.auth()?.createUser(withEmail: email, password: pass, completion: { (user, error) in
+                            if let u = user {
+                                // go to roster
+                                self.performSegue(withIdentifier: "goToRoster", sender: self)
+                            } else {
+                                print("an error occured: \(error)")
+                            }
+                        })
                     }
                 }
             }
+            return
         }
+        print("login failed")
     }
     
-    func confirmedLogin(email: String, pass: String) {
-        // sign in with Firebase
-        FIRAuth.auth()?.signIn(withEmail: email, password: pass, completion: { (user, error) in
-            //Check that user isn't nil
-            if let u = user {
-                // go to roster
-                self.performSegue(withIdentifier: "goToRoster", sender: self)
-            } else {
-                // error
-            }
-        })
-    }
-    
-    func confirmedRegister(email: String, pass: String, confirmation: String) {
-        //sign up with Firebase
-        FIRAuth.auth()?.createUser(withEmail: email, password: pass, completion: { (user, error) in
-            if let u = user {
-                // go to roster
-                self.performSegue(withIdentifier: "goToRoster", sender: self)
-            
-            } else {
-                // error
-            }
-        })
-    }
-    
+    // Dismiss the keyboard when the view is tapped on
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // Dismiss the keyboard when the view is tapped on
         emailTextField.resignFirstResponder()
-        usernameTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
     }
 
