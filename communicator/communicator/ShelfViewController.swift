36805fb5de1f493a91e998a8acac5bf74294c6bb
Morgan Morley Mills <morganmorleymills@Morgans-MacBook-Pro.local>
2017-04-02 20:09:21 -0400

    now events are deleted from the database if the stream or shelf try to load an event that has already happened.

--- communicator/communicator/ShelfViewController.swift
+++ communicator/communicator/ShelfViewController.swift
@@ -21,6 +21,56 @@ class ShelfViewController: UIViewController, UITableViewDelegate, UITableViewDat
 
     @IBOutlet weak var tableView: UITableView!
     
+    func splitDateTime(from dateTime: String) -> Dictionary<String,String> {
+        //Splits dateTime strings into discreet units for comparison
+        let splitInput = dateTime.components(separatedBy: " at ")
+        let date = splitInput[0].components(separatedBy: ", ")
+        let monthAndDay = date[1].components(separatedBy: " ")
+        let month = monthAndDay[0]
+        let day = monthAndDay[1]
+        let year = date[2]
+        let wholeTime = splitInput[1].components(separatedBy: " ")
+        var hourAndMinute = wholeTime[0].components(separatedBy: ":")
+        let minute: Double = Double(hourAndMinute[1])! / 60
+        var hour = ""
+        if wholeTime[1] == "PM" {
+            hour = String(describing: (Double(hourAndMinute[0])! + 12 + minute))
+        } else {
+            hour = String(describing: (Double(hourAndMinute[0])! + minute))
+        }
+        return ["month": month, "day": day, "year": year, "hour": hour]
+    }
+
+    func compareDateTime(with dateTime: String, event: String) -> Bool {
+        // Checks that an event is either currently happening or has yet to happen. If not, it deletes the event from the database.
+        let months = ["January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12]
+        let current = splitDateTime(from: DateFormatter.localizedString(from: Date(), dateStyle: DateFormatter.Style.full, timeStyle: DateFormatter.Style.short))
+        let input = splitDateTime(from: dateTime)
+        if Int(current["year"]!)! <= Int(input["year"]!)!{
+            if months[current["month"]!]! <= months[input["month"]!]! {
+                if Int(current["day"]!)! <= Int(input["day"]!)! {
+                    if Double(current["hour"]!)! <= (Double(input["hour"]!)! + 1) {
+                        return true
+                    }
+                }
+            }
+        }
+        // remove post from database and all references to it
+        ref?.child("posts").child("events").child(event).removeValue()
+        ref?.child("users").observeSingleEvent(of: .value, with: { (snapshot) in
+            if let users = snapshot.value as? Dictionary<String,Dictionary<String,Dictionary<String,String>>> {
+                for (user, data) in users {
+                    for (eventID, _) in data["linked_events"]! {
+                        if eventID == event {
+                            self.ref?.child("users").child(user).child("linked_events").child(eventID).removeValue()
+                        }
+                    }
+                }
+            }
+        })
+        return false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -31,22 +81,26 @@ class ShelfViewController: UIViewController, UITableViewDelegate, UITableViewDat
         // Set the firebase database references:
         ref = FIRDatabase.database().reference()
         userID = FIRAuth.auth()?.currentUser?.uid
+        
         if let userRef = ref?.child("users").child(userID!), let eventsRef = ref?.child("posts").child("events") {
             // post an event titles from the database and observe changes to the database
             userRef.child("linked_events").observeSingleEvent(of: .value, with: { (snapshot) in
                 if let events = snapshot.value as? Dictionary<String,String> {
                     for (eventID, _) in events {
-                        eventsRef.child(eventID).child("details").child("title").observeSingleEvent(of: .value, with: { (snapshot) in
-                            if let postTitle = snapshot.value as? String {
-                                self.postData[postTitle] = eventID
-                                self.postTitles.append(postTitle)
+                        eventsRef.child(eventID).child("details").observeSingleEvent(of: .value, with: { (snapshot) in
+                            if let postDetails = snapshot.value as? Dictionary<String,String> {
+                                // check date against current date incase deletion
+                                let dateTime = postDetails["date_time"]
+                                if self.compareDateTime(with: dateTime!, event: eventID) {
+                                    let postTitle = postDetails["title"]
+                                    self.postData[postTitle!] = eventID
+                                    self.postTitles.append(postTitle!)
+                                }
                                 // Reload the tableView
                                 self.tableView.reloadData()
-
                             }
                         })
                     }
-
                 }
             })
         }
--- communicator/communicator/StreamViewController.swift
+++ communicator/communicator/StreamViewController.swift
@@ -20,6 +20,56 @@ class StreamViewController: UIViewController, UITableViewDelegate, UITableViewDa
 
     @IBOutlet weak var tableView: UITableView!
     
+    func splitDateTime(from dateTime: String) -> Dictionary<String,String> {
+        //Splits dateTime strings into discreet units for comparison
+        let splitInput = dateTime.components(separatedBy: " at ")
+        let date = splitInput[0].components(separatedBy: ", ")
+        let monthAndDay = date[1].components(separatedBy: " ")
+        let month = monthAndDay[0]
+        let day = monthAndDay[1]
+        let year = date[2]
+        let wholeTime = splitInput[1].components(separatedBy: " ")
+        var hourAndMinute = wholeTime[0].components(separatedBy: ":")
+        let minute: Double = Double(hourAndMinute[1])! / 60
+        var hour = ""
+        if wholeTime[1] == "PM" {
+            hour = String(describing: (Double(hourAndMinute[0])! + 12 + minute))
+        } else {
+            hour = String(describing: (Double(hourAndMinute[0])! + minute))
+        }
+        return ["month": month, "day": day, "year": year, "hour": hour]
+    }
+    
+    func compareDateTime(with dateTime: String, event: String) -> Bool {
+        // Checks that an event is either currently happening or has yet to happen. If not, it deletes the event from the database.
+        let months = ["January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12]
+        let current = splitDateTime(from: DateFormatter.localizedString(from: Date(), dateStyle: DateFormatter.Style.full, timeStyle: DateFormatter.Style.short))
+        let input = splitDateTime(from: dateTime)
+        if Int(current["year"]!)! <= Int(input["year"]!)!{
+            if months[current["month"]!]! <= months[input["month"]!]! {
+                if Int(current["day"]!)! <= Int(input["day"]!)! {
+                    if Double(current["hour"]!)! <= (Double(input["hour"]!)! + 1) {
+                        return true
+                    }
+                }
+            }
+        }
+        // remove post from database and all references to it
+        ref?.child("posts").child("events").child(event).removeValue()
+        ref?.child("users").observeSingleEvent(of: .value, with: { (snapshot) in
+            if let users = snapshot.value as? Dictionary<String,Dictionary<String,Dictionary<String,String>>> {
+                for (user, data) in users {
+                    for (eventID, _) in data["linked_events"]! {
+                        if eventID == event {
+                            self.ref?.child("users").child(user).child("linked_events").child(eventID).removeValue()
+                        }
+                    }
+                }
+            }
+        })
+        return false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -37,9 +87,14 @@ class StreamViewController: UIViewController, UITableViewDelegate, UITableViewDa
         eventsRef?.observeSingleEvent(of: .value, with: { (snapshot) in
             if let posts = snapshot.value as? Dictionary<String,Dictionary<String,Dictionary<String,String>>> {
                 for (eventID, data) in posts {
-                    if let eventTitle = data["details"]?["title"] {
-                        self.postData[eventTitle] = eventID
-                        self.postTitles.append(eventTitle)
+                    // check date against current date incase deletion
+                    if let dateTime = data["details"]?["date_time"] {
+                        if self.compareDateTime(with: dateTime, event: eventID) {
+                            if let eventTitle = data["details"]?["title"] {
+                                self.postData[eventTitle] = eventID
+                                self.postTitles.append(eventTitle)
+                            }
+                        }
                     }
                 }
             }
